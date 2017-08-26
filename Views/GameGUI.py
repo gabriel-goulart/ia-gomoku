0d1262862ff69a3357da4a5ecbf80e637186091f
gabriel-goulart <gabriel-r-g@hotmail.com>
2017-08-25 23:04:32 -0300

    carregamento e interações com o tabuleiro funcionando

--- Views/GameGUI.py
+++ Views/GameGUI.py
@@ -2,33 +2,64 @@ from tkinter import *
 from Models.Tabuleiro import Tabuleiro
 
 
-class GameGUI(Frame):
+class GameGUI():
     
     def __init__(self, tabuleiro, controlador):
+        self.tk = Tk()
+        self.tk.geometry('700x700')
+        self.tk.title('GoMoku')
         self.controlador = controlador
         self.tabuleiro = tabuleiro
-        Frame.__init__(self, width=600, height=600)
+        self.mainFrame = Frame(self.tk)
+        # Frame.__init__(self, title= 'Jogo')
         self.start()
         mainloop()
 
     def start(self):
-        self.msg = Label(self, text="GOMOKU")
-        self.msg.pack()
-        self.frame_tabuleiro = Frame(self, width=300, height=300)
+        self.casasList = []
+        
+        title = Label(self.mainFrame, text="GOMOKU")
+        title.pack()
+        self.frame_tabuleiro = Frame(self.mainFrame, width=300, height=300)
+
+        # carregando o tabuleiro do jogo na interface
         self.carregaTabuleiro()
         self.frame_tabuleiro.pack()
-        self.pack()
+
+        self.jogadorDaVezText = StringVar()
+        self.jogadorDaVezLabel = Label(self.mainFrame, textvariable=self.jogadorDaVezText)
+        self.jogadorDaVezText.set("Jogador")
+        self.jogadorDaVezLabel.pack()
+
+        self.mainFrame.pack()
+
+    def setJogadorDaVez(self, jogadorText):
+        self.jogadorDaVezText.set(jogadorText)
+    
+    def casaEscolhida(self, event, linha, coluna):
+        self.setJogada(linha, coluna)
+
+    def setJogada(self, linha, coluna):
+        self.casasList[linha][coluna].configure(background='green')
 
     def carregaTabuleiro(self):
         linha = 0
         
         for row in self.tabuleiro.getEstadoAtual():
             coluna = 0
+            rowList = []
             for casa in row:
-                lb = Frame(self.frame_tabuleiro, width=20, height=20, borderwidth=2)
-                lb.configure(background='green')
-                lb.grid(row=linha, column=coluna)
+                lb1 = Frame(self.frame_tabuleiro, width=30, height=30, relief='sunken')
+                
+                lb1.grid(row=linha, column=coluna)
+                lb2 = Frame(lb1, width=30, height=30, relief='sunken')
+                lb2.configure(background='grey')
+                lb2.bind("<Button-1>", lambda event, arg1=linha, arg2=coluna : self.casaEscolhida(self, arg1, arg2))
+                lb2.grid(padx=5, pady=5)
+                rowList.append(lb2)
                 coluna = coluna + 1
 
+            self.casasList.append(rowList)
             linha = linha + 1
-        
+
+    
